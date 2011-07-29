6ba7cfc74dc8c62dbe85490643d3a9d3962e4fc5
Przemek Hocke <phocke@gmail.com>
2011-07-29 20:24:54 +0100

    initial work on sinatra app

---
+++ home_page.rb
@@ -0,0 +1,16 @@
+class HomePage < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  set :public, Proc.new { File.join(root, "public") }
+  enable :static
+
+  register Sinatra::Warden
+
+  get '/' do
+    #@content_for_layout = "super kurwa stronka"
+    haml :index
+  end
+
+  get '/hi' do
+    "Hello World!"
+  end
+end
