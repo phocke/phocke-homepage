88dd8134dea179652383c55431ab8daf03131389
Przemek Hocke <phocke@gmail.com>
2011-09-05 12:25:12 +0100

    Slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
 /* Author: 
-
+Przemek Hocke
 */
 $(document).ready(function(){
   $('#slider').nivoSlider();
+  $("a[href^='#']").addClass("internal");
+  $("a[href^='#']").click(function(){
+    //$.scrollTo($(this).attr('href'), 1200, {easing: 'easeOutQuad'})
+    $.scrollTo($(this).attr('href'), 1200, {easing: 'swing'})
+  });
 });
 
 
