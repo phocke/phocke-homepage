1bde448e52f9d21b37a1e5f5d51a7b253f183f47
Przemek Hocke <phocke@gmail.com>
2012-01-29 22:34:32 +0000

    hello accordions

--- public/js/script.js
+++ public/js/script.js
@@ -2,33 +2,12 @@
 Przemek Hocke
 */
 $(document).ready(function(){
-  $('#slider').nivoSlider();
   $("a[href^='#']").addClass("internal");
   $("a[href^='#']").click(function(){
-    //$.scrollTo($(this).attr('href'), 1200, {easing: 'easeOutQuad'})
     $.scrollTo($(this).attr('href'), 1200, {easing: 'swing'})
   });
+
+  $('.accordion').accordion({
+    autoHeight: false
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
